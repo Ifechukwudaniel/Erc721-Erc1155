929d306045f15eb044b17313db382607416efc58
Angry Coding <mailidanielifechukwu@gmail.com>
2022-12-06 12:58:26 +0100

    SuperMarioWorld Contact

---
+++ contracts/SuperMarioWorld.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+import "./ERC721.sol";
+
+contract SuperMarioWorld is ERC721 {
+   string public name ; // ERC721 metadata
+   string public symbol; // ERC721 metadata
+   uint256 public tokenCount;
+   mapping(uint256 => string) private _tokenURIs;
+    
+   constructor (string memory _name, string memory _symbol)  {
+       name = _name;
+       symbol= _symbol;
+   }
+   
+   // return a URL that points to the metadata
+   function tokenURI(uint256 tokenId) public view returns (string memory) { // ERC721 metadata
+      require(_owners[tokenId] != address(0), "Token id does not exist");
+       return _tokenURIs[tokenId];
+   }
+
+    //create a new nft in our collection
+   function mint(string memory _tokenURI) public {
+       tokenCount +=1;
+      _balances[msg.sender] +=1;
+      _owners[tokenCount] = msg.sender;
+      _tokenURIs[tokenCount] = _tokenURI;
+      emit Transfer(address(0), msg.sender, tokenCount);
+   }
+
+    function supportsInterface(bytes4 interfaceId) public pure override returns(bool){
+        return interfaceId == 0x80ac58cd || interfaceId == 0x5b5e139f;
+    }
+
+}
