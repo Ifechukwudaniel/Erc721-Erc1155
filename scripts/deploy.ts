86f4ae69e9b60a5b260901184b573e1b2ab67192
Angry Coding <mailidanielifechukwu@gmail.com>
2022-12-06 15:20:03 +0100

    deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+   const SuperMarioWorld = await ethers.getContractFactory("SuperMarioWorldOZ")
+   const superMarioWorld = SuperMarioWorld.deploy("Super Mario World OZ","SPRMO")
+   await (await superMarioWorld).deployed()
+   console.log("contract was deployed  to ", (await superMarioWorld).address)
+    await (await superMarioWorld).mint("https://ipfs.io/ipfs/QmeX9DWdk4nt46CHR5AsezRywjg6AX8km8aic5aqNikA5e")
+    await (await superMarioWorld).mint("https://ipfs.io/ipfs/QmeX9DWdk4nt46CHR5AsezRywjg6AX8km8aic5aqNikA5e")
 }
 
 // We recommend this pattern to be able to use async/await everywhere
