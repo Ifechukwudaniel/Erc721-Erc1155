da078fe738edaba3b87a34816fd4d2f4825af433
Angry Coding <mailidanielifechukwu@gmail.com>
2022-12-07 14:03:04 +0100

    configured mumbai network  and installed packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
+import "dotenv/config"
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    mumbai:{
+      url:process.env.MUMBAI_RPC,
+      accounts:[process.env.PRIVATE_KEY || ""]
+    }
+  }
 };
 
 export default config;
